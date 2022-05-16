728c27e85920065771640985214fc68f3824b30a
Sergey Andreev <wolf22394@gmail.com>
2022-05-16 18:19:58 +0300

    My first homework

---
+++ dz2.loc/index.php
@@ -0,0 +1,12 @@
+<?php
+$hello = "Hello Word!";
+echo $hello;
+
+echo "Предопределенные переменные";
+echo 'money_format — Форматирует число как денежную величину';
+echo $money_format;
+
+$hel = "one";
+$Hel = "two";
+
+echo $hel . "=" . $Hel;
