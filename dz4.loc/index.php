f48d789852ca0e39e0ffdb0acf22f38f58d645d5
Sergey Andreev <wolf22394@gmail.com>
2022-05-17 23:16:53 +0300

    Task4-task2

--- dz4.loc/index.php
+++ dz4.loc/index.php
@@ -12,3 +12,20 @@ if($even % 2 != 0) {
 	echo "Произошла ошибка";
 }
 echo "<br>";
+echo 'Дано два целых числа.
+Проверить делится ли первое на второе.
+Вывести на экран сообщение об этом, а также остаток (если он есть).';
+$number1 = rand(0,100);
+$number2 = rand(0,100);
+echo "<br>";
+echo 'Результат:';
+if ($number1 > 0 && $number2 > 0) {
+	if($number1 > $number2) {
+		echo "Делиться: $number1" . '/' . "$number2 = " . round(($number1 / $number2), 0) . ' остаток от деления ' . ($number1 % $number2 != 0 ?  $number1 % $number2 : "нет");
+	} else {
+		echo "Делиться: $number1" . '/' . "$number2 = " . ($number1 / $number2);
+	}
+} else {
+	echo "Ноль делить , как и на ноль = нельзя" . '$number1= ' . $number1. '  , $number2= ' .$number2;
+}
+echo "<br>";
