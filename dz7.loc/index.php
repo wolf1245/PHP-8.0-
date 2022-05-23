4e07838282bea3c2dc784d4a40fd9b38efa00c6f
Sergey Andreev <wolf22394@gmail.com>
2022-05-23 19:22:33 +0300

    Task7-task1

---
+++ dz7.loc/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Задание 1
+*Создайте функцию возведения числа в степень.
+*Число и степень передаются аргументом.
+ */
+function degree($number, $degree) 
+{
+	return $number ** $degree;
+}
+echo degree(2, 2);
+echo "<br>";
