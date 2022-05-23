f1229068feb78fab73468b90782521979d498776
Sergey Andreev <wolf22394@gmail.com>
2022-05-23 19:29:55 +0300

    Task7-task2

--- dz7.loc/index.php
+++ dz7.loc/index.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 /**
  * Задание 1
 *Создайте функцию возведения числа в степень.
@@ -9,4 +10,16 @@ function degree($number, $degree)
 	return $number ** $degree;
 }
 echo degree(2, 2);
+echo "<br>";
+/**
+ * Задание 2
+*Создайте функцию rgb(), которая будет принимать три числовых аргумента и возвращать строку
+*вида “rgb(23,100,134)”.
+*Если аргументы не заданы, считать их равными нулю.
+ */
+$rgb = function (int $red = 0, int $green = 0, int $blue = 0) : string
+{
+	return "rgb($red, $green, $blue)";
+};
+echo $rgb();
 echo "<br>";
